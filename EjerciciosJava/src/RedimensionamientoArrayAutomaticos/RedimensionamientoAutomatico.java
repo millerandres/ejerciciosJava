01d305221947a2fff2c8e89379cf32ce9f0c7226
MillerCaro <ingenierogom@gmail.com>
2024-06-25 16:28:55 -0500

    redimensionado automatico

---
+++ EjerciciosJava/src/RedimensionamientoArrayAutomaticos/RedimensionamientoAutomatico.java
@@ -0,0 +1,15 @@
+package RedimensionamientoArrayAutomaticos;
+
+import java.util.ArrayList;
+
+public class RedimensionamientoAutomatico {
+    public static void main(String[] args) {
+
+        ArrayList<Integer> enteros = new ArrayList<>();
+
+        for (int i =0;i<10000;i++){
+            enteros.add(i);
+        }
+
+    }
+}
